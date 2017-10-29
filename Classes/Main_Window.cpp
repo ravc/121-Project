96b59f278bf4f0f50acd308f838138c266f91e70
luke-witt17 <33095137+luke-witt17@users.noreply.github.com>
2017-10-29 15:08:22 -0500

    added the first 5 menus and options to the GUI

--- Classes/Main_Window.cpp
+++ Classes/Main_Window.cpp
@@ -2,17 +2,161 @@
 # define Main_Window_H
 #include "Main_Window.h"
 
-main_window::main_window(Point xy, int w, int h, const string& title): //setting up the window and buttons
+Main_Window::Main_Window(Point xy, int w, int h, const string& title): //setting up the window and buttons
     Window(xy,w,h,title),
     quit_button(Point(x_max()-70,0), 70, 20, "Quit", cb_quit),
-    {
-        attach(quit_button);
+    cust_menu_button(Point(0,0),100,20,"Customer Menu",cb_custMenuButton)
+	customerMenu(Point(0,0),100,20,Menu::vertical,"Customer Menu"),
+	driver_menu_button(Point(100, 0), 100, 20, "Driver Menu",cb_driverMenuButton)
+	driverMenu(Point(100,0),100,20,Menu::vertical,"Driver Menu"),
+	place_menu_button(Point(200, 0), 100, 20, "Place Menu",cb_placeMenuButton)
+	placeMenu(Point(200,0),100,20,Menu::vertical,"Place Menu"),
+	ride_menu_button(Point(300, 0), 100, 20, "Ride Menu",cb_rideMenuButton)
+	rideMenu(Point(300,0),100,20,Menu::vertical,"Ride Menu"),
+	display_menu_button(Point(400, 0), 100, 20, "Display Menu"cb_displayMenuButton)
+	displayMenu(Point(400,0),100,20,Menu::vertical,"Display"),
+	{
+		customerMenu.attach(new Button(Point(0, 0), 0, 0, "Add Customer",cb_addCustomer));
+		customerMenu.attach(new Button(Point(0, 0), 0, 0, "Remove Customer", cb_removeCustomer));
+		driverMenu.attach(new Button(Point(0, 0), 0, 0, "Add Driver", cb_addDriver));
+		driverMenu.attach(new Button(Point(0, 0), 0, 0, "Remove Driver", cb_removeDriver));
+		placeMenu.attach(new Button(Point(0, 0), 0, 0, "Add Place", cb_addPlace));
+		placeMenu.attach(new Button(Point(0, 0), 0, 0, "Remove Place", cb_removePlace));
+		rideMenu.attach(new Button(Point(0, 0), 0, 0, "Pay Balance", cb_payBalance));
+		rideMenu.attach(new Button(Point(0, 0), 0, 0, "Request Ride", cb_requestRide));
+		displayMenu.attach(new Button(Point(0, 0), 0, 0, "Display Customers", cb_displayCustomers));
+		displayMenu.attach(new Button(Point(0, 0), 0, 0, "Display Drivers", cb_displayDrivers));
+		displayMenu.attach(new Button(Point(0, 0), 0, 0, "Display Places", cb_displayPlaces));
+		attach(quit_button);
+		attach(customerMenu);
+		attach(driverMenu);
+		attach(placeMenu);
+		attach(rideMenu);
+		attach(displayMenu);
+
     }
 
 void Main_Window::quit(){
     hide()
 }
-
 void Main_Window::cb_quit(Address, Address pw){
     reference_to<main_window>(pw).quit();
 }
+
+void Main_Window::custMenuButton() {
+	cust_menu_button.hide();
+	customerMenu.show();
+}
+void Main_Window::cb_custMenuButton(Address, Address pw) {
+	reference_to<main_window>(pw).custMenuButton();
+}
+
+void Main_Window::driverMenuButton() {
+	driver_menu_button.hide();
+	driverMenu.show();
+}
+void Main_Window::cb_driverMenuButton(Address, Address pw) {
+	reference_to<main_window>(pw).driverMenuButton();
+}
+
+void Main_Window::placeMenuButton() {
+	place_menu_button.hide();
+	placeMenu.show();
+}
+void Main_Window::cb_placeMenuButton(Address, Address pw) {
+	reference_to<main_window>(pw).placeMenuButton();
+}
+
+void Main_Window::rideMenuButton() {
+	ride_menu_button.hide();
+	rideMenu.show();
+}
+void Main_Window::cb_rideMenuButton(Address, Address pw) {
+	reference_to<main_window>(pw).rideMenuButton();
+}
+
+void Main_Window::displayMenuButton() {
+	display_menu_button.hide();
+	displayMenu.show();
+}
+void Main_Window::cb_displayMenuButton(Address, Address pw) {
+	reference_to<main_window>(pw).displayMenuButton();
+}
+
+void Main_Window::addCustomer() {
+
+}
+void Main_Window::cb_addCustomer(Address, Address pw) {
+	reference_to<main_window>(pw).addCustomer();
+}
+
+void Main_Window::removeCustomer() {
+
+}
+void Main_Window::cb_removeCustomer(Address, Address pw) {
+	reference_to<main_window>(pw).removeCustomer();
+}
+
+void Main_Window::addDriver() {
+
+}
+void Main_Window::cb_addDriver(Address, Address pw) {
+	reference_to<main_window>(pw).addDriver();
+}
+
+void Main_Window::removeDriver() {
+
+}
+void Main_Window::cb_removeDriver(Address, Address pw) {
+	reference_to<main_window>(pw).removeDriver();
+}
+
+void Main_Window::addPlace() {
+
+}
+void Main_Window::cb_addPlace(Address, Address pw) {
+	reference_to<main_window>(pw).addPlaces();
+}
+
+void Main_Window::removePlace() {
+
+}
+void Main_Window::cb_removePlace(Address, Address pw) {
+	reference_to<main_window>(pw).removePlace();
+}
+
+void Main_Window::payBalance() {
+
+}
+void Main_Window::cb_payBalance(Address, Address pw) {
+	reference_to<main_window>(pw).payBalance();
+}
+
+void Main_Window::requestRide() {
+
+}
+void Main_Window::cb_requestRide(Address, Address pw) {
+	reference_to<main_window>(pw).requestRide();
+}
+
+void Main_Window::displayCustomers() {
+
+}
+void Main_Window::cb_displayCustomers(Address, Address pw) {
+	reference_to<main_window>(pw).displayCustomers();
+}
+
+void Main_Window::displayDrivers() {
+
+}
+void Main_Window::cb_displayDrivers(Address, Address pw) {
+	reference_to<main_window>(pw).displayDrivers();
+}
+
+void Main_Window::displayPlaces() {
+
+}
+void Main_Window::cb_displayPlaces(Address, Address pw) {
+	reference_to<main_window>(pw).displayPlaces();
+}
+
