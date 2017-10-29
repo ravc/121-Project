f491dc755d967422ade5e0e8539db0372cfb16fd
luke-witt17 <33095137+luke-witt17@users.noreply.github.com>
2017-10-29 14:15:21 -0500

    Added code to the customer.cpp .h

---
+++ Classes/Customer.cpp
@@ -0,0 +1,11 @@
+#include "Customer.h"
+
+customers::customers(string inputName, double inputAccountbalance) :
+	name(inputName), accountBalance(inputAccountbalance), currentLocation(Place_info("", "", emptyKeys, Geo_loc(0, 0))) {}
+int customers::getAccountNumber() { return accountNumber; }
+double customers::getAccountBalance() { return accountBalance; }
+void customers::addAmountOwed(double amount) { accountBalance -= amount; }
+Place_info customers::getCurrentLocation() { return currentLocation; }
+void customers::makePayment(double payment) { accountBalance += payment; }
+string customers::getName() { return name; }
+void customers::changeLocation(Place_info loc) { currentLocation = loc; }
