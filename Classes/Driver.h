25875631d2752679d3d701c3192eac1369266238
Ruben Vazquez-Chapa <ravc@protonmail.com>
2017-10-29 14:28:12 -0500

    tried to comiple and made changes

--- Classes/Driver.h
+++ Classes/Driver.h
@@ -1,7 +1,7 @@
 #ifndef drivers_H
 #define drivers_H
-#include "std_lib_facilities_4.h";
-#include "Geo_Loc.h";
+#include "std_lib_facilities_4.h"
+#include "Geo_Loc.h"
 //The drivers class creates a driver to be used in the myRide problem
 //It contains information about the driver such as user number, name, account balance and current location
 class drivers {
