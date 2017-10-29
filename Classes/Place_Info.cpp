65dfef192489b286222e3ca47315a48d9d9121a9
Ruben Vazquez-Chapa <ravc@protonmail.com>
2017-10-29 15:37:51 -0500

    adding variables inside of h files

--- Classes/Place_Info.cpp
+++ Classes/Place_Info.cpp
@@ -1,12 +1,14 @@
 #include "std_lib_facilities_4.h"
 #include "Place_Info.h"
 //The Place_info class holds all of the information about a certain place. 
-//This consists of the geo_loc of their coordinates as well as the name of the place and certain keys that could be used to describe it
+//This consists of the Geo_Loc of their coordinates as well as the name of the place and certain keys that could be used to describe it
 
-Place_info::Place_info(string inputName, string inputAddress, vector<string> inputTags, Geo_loc inputLocation) :
+vector<string> tags;
+
+Place_info::Place_info(string inputName, string inputAddress, vector<string> inputTags, Geo_Loc inputLocation) :
 name(inputName), address(inputAddress), tags(inputTags), location(inputLocation) {}
 string Place_info::getName() { return name; }
 string Place_info::getAddress() { return address; }
 vector<string> Place_info::getTags() { return tags; }
-Geo_loc Place_info::getLocation() { return location; }
+Geo_Loc Place_info::getLocation() { return location; }
 
