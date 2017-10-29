1cd72658ceba2ab662cbdf61e0cab2b8e7a26b9b
Ruben Vazquez-Chapa <ravc@protonmail.com>
2017-10-29 13:46:43 -0500

    Start of the Main file

---
+++ Main.cpp
@@ -0,0 +1,9 @@
+#include "Classes/std_lib_facilities_4.h"
+#include "Classes/My_Ride.h"
+#include "Classes/Place_Info.h"
+#include "Classes/Geo_Loc.h"
+#include "Classes/Main_WIndow.h"
+
+int main(){
+    
+}
